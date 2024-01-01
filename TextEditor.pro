3167b7aacfc2fffc7ec7f7f629588273ab2f1172
Stanislav Bulgakov <38434332+sbulgakov@users.noreply.github.com>
2024-01-01 17:50:12 +0300

    added application icon

--- TextEditor.pro
+++ TextEditor.pro
@@ -9,3 +9,6 @@ SOURCES += main.cpp \
 
 HEADERS += \
     textedit.h
+
+RESOURCES += \
+    icons.qrc
--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <QApplication>
+#include <QIcon>
 
 #include "textedit.h"
 
 int main(int argc, char **argv)
 {
   QApplication app(argc, argv);
+  app.setWindowIcon(QIcon(":/images/icon.png"));
   
   TextEdit w;
   w.show();
