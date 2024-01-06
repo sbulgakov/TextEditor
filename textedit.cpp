c2e4d98697c02d13c3a8e6ca2d9a416ff6abfeda
Stanislav Bulgakov <38434332+sbulgakov@users.noreply.github.com>
2024-01-06 04:31:14 +0300

    added line numbers

--- textedit.cpp
+++ textedit.cpp
@@ -1,18 +1,84 @@
 #include "textedit.h"
 
 #include <QPlainTextEdit>
+#include <QScrollBar>
+#include <QTextBlock>
 #include <QMessageBox>
 #include <QTextStream>
+#include <QPainter>
 
-TextEdit::TextEdit(QWidget *parent) : QWidget(parent)
+TextEdit::TextEdit(QWidget *parent) : QWidget(parent),
+  pos(0), blockCount(0)
 {
   edit = new QPlainTextEdit(this);
   edit->setLineWrapMode(QPlainTextEdit::NoWrap);
+  
+  lineNumbers = new LineNumbers(this);
+  lineNumbers->setNumbers(QStringList() << "1");
+  
+  connect(edit,
+#if (QT_VERSION < QT_VERSION_CHECK(5,0,0))
+          SIGNAL(updateRequest(QRect,int)),
+#else
+          &QPlainTextEdit::updateRequest,
+#endif
+          this,
+#if (QT_VERSION < QT_VERSION_CHECK(5,0,0))
+          SLOT(updateLineNumbers(QRect,int)));
+#else
+          QOverload<const QRect&,int>::of(&TextEdit::updateLineNumbers));
+#endif
+  
+  connect(edit->verticalScrollBar(),
+#if (QT_VERSION < QT_VERSION_CHECK(5,0,0))
+          SIGNAL(valueChanged(int)),
+#else
+          &QScrollBar::valueChanged,
+#endif
+          this,
+#if (QT_VERSION < QT_VERSION_CHECK(5,0,0))
+          SLOT(updateLineNumbers(int)));
+#else
+          QOverload<int>::of(&TextEdit::updateLineNumbers));
+#endif
+  
+  connect(edit,
+#if (QT_VERSION < QT_VERSION_CHECK(5,0,0))
+          SIGNAL(cursorPositionChanged()),
+#else
+          &QPlainTextEdit::cursorPositionChanged,
+#endif
+          this,
+#if (QT_VERSION < QT_VERSION_CHECK(5,0,0))
+          SLOT(updateLineNumbers()));
+#else
+          QOverload<>::of(&TextEdit::updateLineNumbers));
+#endif
 }
 
 TextEdit::~TextEdit()
 {
 
+}
+
+int TextEdit::lineNumbersWidth() const
+{
+  int max = 1;
+  int num = qMax(1,edit->blockCount());
+  
+  while(num)
+  {
+    max +=  1;
+    num /= 10;
+  }
+  
+#if (QT_VERSION < QT_VERSION_CHECK(5,0,0))
+  int wid = 3 + fontMetrics().width(QLatin1Char('9')) * max;
+#else
+  int wid = 3 + fontMetrics().horizontalAdvance(QLatin1Char('9')) * max;
+#endif
+  
+  return wid;
 }
 
 bool TextEdit::Open(const QString& fileName)
@@ -35,6 +101,10 @@ bool TextEdit::Open(const QString& fileName)
     edit->setPlainText(in.readAll());
     
     file.close();
+    
+    updateLineNumbersWidth(edit->blockCount());
+    lineNumbers->setScroll(0);
+    lineNumbers->update();
   }
   return true;
 }
@@ -61,7 +131,127 @@ bool TextEdit::Save(const QString& fileName)
     return true;
 }
 
+void TextEdit::updateLineNumbersWidth(int newBlockCount)
+{
+  QTextDocument *doc = edit->document();
+  int num = 1;
+  QStringList numbers;
+  QTextBlock block = doc->findBlockByNumber(num);
+  
+  while(num <= newBlockCount)
+  {
+    if(block.isVisible()) numbers << QString::number(num);
+    block = doc->findBlockByNumber(++num);
+  }
+  
+  lineNumbers->setNumbers(numbers);
+  
+  int wid = lineNumbersWidth();
+  QSize size = edit->size();
+  size.setWidth(width()-wid);
+  edit->resize(size);
+  edit->move(wid,0);
+  lineNumbers->resize(wid,size.height());
+}
+
+void TextEdit::updateLineNumbers(const QRect &rect, int dy)
+{
+  Q_UNUSED(rect)
+  
+  if(dy)
+  {
+    lineNumbers->setScroll(lineNumbers->getScroll() + dy);
+    lineNumbers->update();
+  }
+  else if(pos != edit->verticalScrollBar()->value())
+  {
+    updateLineNumbers();
+  }
+}
+
+void TextEdit::updateLineNumbers(int val)
+{
+  pos = val;
+  
+  if(val == 0) lineNumbers->setScroll(3);
+  else         lineNumbers->setScroll(-1);
+  
+  int ht  = fontMetrics().height();
+  lineNumbers->setScroll(lineNumbers->getScroll()
+                         -3
+                         -ht*val
+                        );
+}
+
+void TextEdit::updateLineNumbers()
+{
+  if(blockCount != edit->blockCount())
+  {
+    blockCount = edit->blockCount();
+    updateLineNumbersWidth(blockCount);
+    lineNumbers->update();
+  }
+  
+  if(pos != edit->verticalScrollBar()->value())
+  {
+    updateLineNumbers(edit->verticalScrollBar()->value());
+  }
+}
+
 void TextEdit::resizeEvent(QResizeEvent *event)
 {
-  edit->resize(event->size());
+  int wid = lineNumbersWidth();
+  QSize size = event->size();
+  
+  size.setWidth(size.width()-wid);
+  edit->resize(size);
+  edit->move(wid,0);
+  lineNumbers->resize(wid,size.height());
+}
+
+//--------------------------------------------------------------------
+
+LineNumbers::LineNumbers(TextEdit *parent)
+    : QWidget(parent), edit(parent), scroll(0)
+{
+
+}
+
+void LineNumbers::setNumbers(const QStringList& numbers)
+{
+  this->numbers = numbers;
+}
+
+int LineNumbers::getScroll() const
+{
+  return scroll;
+}
+
+void LineNumbers::setScroll(int scroll)
+{
+  this->scroll = scroll;
+}
+
+void LineNumbers::paintEvent(QPaintEvent *event)
+{
+  Q_UNUSED(event)
+  
+  QPainter p(this);
+  
+  int wd  = width();
+  int ht  = fontMetrics().height();
+  int top = 3 + 2
+#ifdef Q_OS_WIN
+              + 1
+#endif
+              + scroll;
+  
+#if (QT_VERSION < QT_VERSION_CHECK(5,0,0))
+  foreach (QString num, numbers) {
+#else
+  for(QString num : numbers) {
+#endif
+    p.drawText(0,top,wd,ht,Qt::AlignRight,num);
+    top += ht;
+  }
 }
--- textedit.h
+++ textedit.h
@@ -5,22 +5,54 @@
 
 class QPlainTextEdit;
 
+class LineNumbers;
+
 class TextEdit : public QWidget
 {
     Q_OBJECT
     
     QPlainTextEdit *edit;
+    LineNumbers *lineNumbers;
+    int pos;
+    int blockCount;
     
 public:
     TextEdit(QWidget *parent = 0);
    ~TextEdit();
     
+    int lineNumbersWidth() const;
+    
 public slots:
     bool Open(const QString& fileName);
     bool Save(const QString& fileName);
     
+private slots:
+    void updateLineNumbersWidth(int newBlockCount);
+    void updateLineNumbers(const QRect &rect, int dy);
+    void updateLineNumbers(int val);
+    void updateLineNumbers();
+    
 protected:
     void resizeEvent(QResizeEvent *event);
+};
+
+//--------------------------------------------------------------------
+
+class LineNumbers: public QWidget
+{
+    TextEdit *edit;
+    QStringList numbers;
+    int scroll;
+    
+public:
+    LineNumbers(TextEdit *parent = 0);
+    void setNumbers(const QStringList& numbers);
+    
+    inline int  getScroll() const;
+    inline void setScroll(int scroll);
+    
+protected:
+    void paintEvent(QPaintEvent *event);
 };
 
 #endif // TEXTEDIT_H
